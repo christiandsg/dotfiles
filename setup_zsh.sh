f475621a97d2ccaae6f49393f93cbaccc78557ae
christiandsg <christian.desousa@gmail.com>
2014-12-09 07:34:51 -0800

    Adding .oh-my-zsh (with default config)

---
+++ setup_zsh.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# Simple script to setup zsh goodies
+
+set -e
+
+echo "#### ZSH Config ####"
+
+HOME_ZSH=~/.oh-my-zsh
+HOME_ZSHRC=~/.zshrc
+
+clear_zsh_configs(){
+  echo "=======> Creating simbolic link to .oh-my-zsh"
+  rm -rf $HOME_ZSH
+  ln -s $PWD/.oh-my-zsh $HOME_ZSH
+  echo "=======> Clearing .zshrc"
+  rm $HOME_ZSHRC
+  touch $HOME_ZSHRC
+}
+
+get_base_zshrc(){
+  echo "=======> Pulling base .zshrc"
+  #curl -LSso $ZSHRC https://raw.githubusercontent.com/christiandsg/dotfiles/master/.zshrc.base
+  cp $PWD/.zshrc $HOME_ZSHRC
+}
+
+# Commands
+clear_zsh_configs
+get_base_zshrc
